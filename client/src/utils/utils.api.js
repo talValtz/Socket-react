4a041fdc3057d1c16eb5f5f50d6c8d5692fbbdfe
talValtz <tvaltzer@gmail.com>
2025-03-02 19:24:29 +0200

    Added API URL to .env

--- client/src/utils/utils.api.js
+++ client/src/utils/utils.api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+VITE_API_URL="https://socket-react-1.onrender.com"
 
-const API_URL = "http://localhost:3000"; // Update according to the server address
+//const API_URL = "http://localhost:3000"; // Update according to the server address
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
   baseURL: API_URL,
